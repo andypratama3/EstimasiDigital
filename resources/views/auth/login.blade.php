28950f89dd12cc2e8a8071f30fdf0032463de418
andypratama3 <andypratama1211@gmail.com>
2025-11-25 21:13:00 +0800

    Update Home

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,8 +26,8 @@
         <div class="mx-0 row w-100">
           <div class="mx-auto col-lg-4">
             <div class="px-4 py-5 text-left auth-form-light px-sm-5">
-              <div class="brand-logo">
-                <img src="{{ asset('assets/images/logo.svg')}}" alt="logo">
+              <div class="brand-logo"style="text-align: center !important;">
+                <img src="{{ asset('assets_home/images/logo1.png')}}" alt="logo" >
               </div>
               <h4>Hello! let's get started</h4>
               <h6 class="font-weight-light">Sign in to continue.</h6>
@@ -42,23 +42,7 @@
                 <div class="mt-3">
                   <button class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" type="submit">SIGN IN</button>
                 </div>
-                <div class="my-2 d-flex justify-content-between align-items-center">
-                  <div class="form-check">
-                    <label class="form-check-label text-muted">
-                      <input type="checkbox" class="form-check-input">
-                      Keep me signed in
-                    </label>
-                  </div>
-                  <a href="#" class="text-black auth-link">Forgot password?</a>
-                </div>
-                <div class="mb-2">
-                  <button type="button" class="btn btn-block btn-facebook auth-form-btn">
-                    <i class="mr-2 ti-facebook"></i>Connect using facebook
-                  </button>
-                </div>
-                <div class="mt-4 text-center font-weight-light">
-                  Don't have an account? <a href="register.html" class="text-primary">Create</a>
-                </div>
+
               </form>
             </div>
           </div>
